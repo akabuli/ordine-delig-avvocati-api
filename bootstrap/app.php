98e233e495b268435309cbb3e516d65f8b7ee1ff
Ayat <ayatullahhaidari1@gmail.com>
2025-09-14 18:38:48 +0200

    setup the dynamic enviroment variables

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,27 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
+// Force detection from current environment only
+
+
+$environment = getenv('APP_ENV') ?? null;
+
+// Check if user selected an environment
+if (!$environment) {
+    die('Error: Please specify an environment type using APP_ENV.\nExample: $env:APP_ENV="test"; php artisan serve\nAvailable environments: development, test, production' . PHP_EOL);
+}
+
+if (!file_exists(__DIR__ . '/../.env.' . $environment)) {
+    die('Environment file not found');
+}
+
+
+// echo $environment . PHP_EOL;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
